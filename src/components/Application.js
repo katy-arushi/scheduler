90f425c6aa44aae81d764ca438348b797e80e39d
Arushi Katyal <katy.arushi@gmail.com>
2021-11-12 16:12:46 -0500

    change controlled list props names to be more semantic

--- src/components/Application.js
+++ src/components/Application.js
@@ -25,7 +25,11 @@ const days = [
 ];
 
 export default function Application(props) {
+
+  //setDay state, to be passed down to DayListItem
+
   const [day, setDay] = useState("Monday");
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -38,8 +42,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            day={day}
-            setDay={setDay}
+            value={day}
+            onChange={setDay}
           />
         </nav>
         <img
