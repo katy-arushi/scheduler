106b8a711371d06371e59e5ed7409e3540de10bb
Arushi Katyal <katy.arushi@gmail.com>
2021-11-12 00:32:23 -0500

    add story for InterviewerListItem

--- stories/index.js
+++ stories/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 
+// storybook imports
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 
+// styles
 import "index.scss";
 
+// components
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
+import InterviewerListItem from "components/InterviewerListItem";
 
+// Button
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -24,7 +29,8 @@ storiesOf("Button", module)
       Disabled
     </Button>
   ));
-
+  
+// DayListItem
 storiesOf("DayListItem", module) //Initiates Storybook and registers our DayListItem component
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -54,6 +60,7 @@ const days = [
   },
 ];
 
+// DayList
 storiesOf("DayList", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
@@ -66,4 +73,39 @@ storiesOf("DayList", module)
   ))
   .add("Wednesday", () => (
       <DayList days={days} day={"Wednesday"} setDay={action("setDay")} />
+  ));
+
+// InterviewerListItem
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png"
+};
+
+storiesOf("InterviewerListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Unselected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+    />
+  ))
+  .add("Selected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected
+    />
+  ))
+  .add("Clickable", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      setInterviewer={action("setInterviewer")}
+    />
   ));
