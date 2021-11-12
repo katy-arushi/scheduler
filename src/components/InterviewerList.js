7abe6c2d179841f2c6a069ec7c2939af18365dee
Arushi Katyal <katy.arushi@gmail.com>
2021-11-12 15:25:46 -0500

    refactor props

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -3,16 +3,17 @@ import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
 
 export default function InterviewerList(props) {
-
-  console.log(props);
-
+  
   const interviewersList = props.interviewers.map(interviewer => {
-    return <InterviewerListItem
-      key={interviewer.id}
-      name={interviewer.name}
-      avatar={interviewer.avatar}
-      selected={interviewer.id === props.interviewer}
-      setInterviewer={() => props.setInterviewer(interviewer.id)}/>
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.interviewer}
+        setInterviewer={() => props.setInterviewer(interviewer.id)}
+      />
+    );
   });
 
   return (
