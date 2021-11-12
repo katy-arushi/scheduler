66e1e2bb645424827d28bf5c9e3607576d4f984f
Arushi Katyal <katy.arushi@gmail.com>
2021-11-11 22:40:26 -0500

    fix bug in props

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,7 @@ import React from "react";
 import DayListItem from "./DayListItem";
 
 export default function DayList(props) {
-  const DayListItems = prop.days.map(day => {
+  const DayListItems = props.days.map(day => {
     return <DayListItem
       key={day.id}
       name={day.name}
