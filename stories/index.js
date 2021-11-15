4aa1f29610da977f9543344aabb6d94f7093be41
Arushi Katyal <katy.arushi@gmail.com>
2021-11-14 23:56:24 -0500

    add form story

--- stories/index.js
+++ stories/index.js
@@ -165,5 +165,5 @@ storiesOf("Appointment", module)
   .add("Confirm", () => <Confirm message={"Delete the appointment?"} onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
   .add("Status", () => <Status message={"Deleting"} />)
   .add("Error", () => <Error message={"Could not delete appointment."} onClose={action("onClose")} />)
-  .add("Edit", () => <Form student={"Arushi"} interviewer={2} onSave={action("onSave")} onCancel={action("onCancel")} interviewers={interviewers} />)
+  .add("Edit", () => <Form student={"Arushi"} interviewer={interviewer} onSave={action("onSave")} onCancel={action("onCancel")} interviewers={interviewers} />)
   .add("Create", () => <Form onSave={action("onSave")} onCancel={action("onCancel")} interviewers={interviewers} />);
