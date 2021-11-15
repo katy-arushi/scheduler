c29698f811ef369b7fda05d918f7b203dd12413f
Arushi Katyal <katy.arushi@gmail.com>
2021-11-14 23:51:40 -0500

    add form story

--- stories/index.js
+++ stories/index.js
@@ -23,6 +23,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form"
 
 // Button
 storiesOf("Button", module)
@@ -163,4 +164,6 @@ storiesOf("Appointment", module)
   .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")} />)
   .add("Confirm", () => <Confirm message={"Delete the appointment?"} onConfirm={action("onConfirm")} onCancel={action("onCancel")} />)
   .add("Status", () => <Status message={"Deleting"} />)
-  .add("Error", () => <Error message={"Could not delete appointment."} onClose={action("onClose")} />);
+  .add("Error", () => <Error message={"Could not delete appointment."} onClose={action("onClose")} />)
+  .add("Edit", () => <Form student={"Arushi"} interviewer={2} onSave={action("onSave")} onCancel={action("onCancel")} interviewers={interviewers} />)
+  .add("Create", () => <Form onSave={action("onSave")} onCancel={action("onCancel")} interviewers={interviewers} />);
