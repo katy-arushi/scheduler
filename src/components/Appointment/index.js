f4c3f6f50e1e0932879a0b8945f89fce2b7f9201
Arushi Katyal <katy.arushi@gmail.com>
2021-11-15 20:00:41 -0500

    add components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -9,17 +9,13 @@ import Empty from "./Empty";
 
 
 export default function Appointment(props) {
-
-  const appointments = (time) => {
-    if (time === undefined) {
-      return "No Appointments"
-    }
-    return `Appointment at ${time} `;
-  }
   
   return (
     <article className="appointment">
-      {appointments(props.time)}
+      <Header
+        time={props.time}>
+      </Header>
+      {props.interview ? <Show/> : <Empty/> }
     </article>
   );
 } 
