7321189f15caf65bc380ae8c3bb70e9b2ca0f03f
Arushi Katyal <katy.arushi@gmail.com>
2021-11-15 19:11:02 -0500

    import components

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import "./styles.scss";
 import classNames from "classnames";
 
+// component imports
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
+
 export default function Appointment(props) {
 
   const appointments = (time) => {
