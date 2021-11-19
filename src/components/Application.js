d28f1cc3c7590a6e58fac4aecab11f6030052552
Arushi Katyal <katy.arushi@gmail.com>
2021-11-19 01:17:01 -0500

    use useEffect to get API data

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,28 +1,12 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "components/Application.scss";
+import axios from "axios";
+
 // components
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 
 // data
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
 const appointments = {
   "1": {
     id: 1,
@@ -65,8 +49,15 @@ const appointments = {
 export default function Application(props) {
 
   // states
+  const [days, setDays] = useState([]);
+
   //setDay state, to be passed down to DayListItem
   const [day, setDay] = useState("Monday");
+
+  useEffect(() => {
+    axios.get("http://localhost:8001/api/days")
+    .then((response) => (setDays([...response.data])))
+  }, []);
 
   const appointmentsArray = Object.values(appointments).map(appointment => {
     return (
