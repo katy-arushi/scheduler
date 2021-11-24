43f2738a0ee003a4e39cc6355faeca521ab6526f
Arushi Katyal <katy.arushi@gmail.com>
2021-11-24 01:46:56 -0500

    finish implementing getInterviewersForDay function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -35,5 +35,17 @@ export function getInterview(state, interview) {
 }
 
 export function getInterviewersForDay(state, day) {
+  const selectedDay = state.days.filter(eachDay => eachDay.name === day); //    [ { id: 1, name: 'Monday', appointments: [ 1, 2, 3 ] } ]
+
+  const interviewers = [];
+
+  if (selectedDay[0]) { // { id: 1, name: 'Monday', appointments: [ 1, 2, 3 ] }
+    const selectedDayInterviewers = selectedDay[0].interviewers; // [1, 2, 3]
+
+    for (const interviewerID of selectedDayInterviewers) { // 1
+      interviewers.push(state.interviewers[interviewerID]) // { id: 1, time: "12pm", interview: null }
+    }
+  } 
+  return interviewers;
   
 }
