aa24dc09ac2521ded9dc90599531ce7979ee3784
Arushi Katyal <katy.arushi@gmail.com>
2021-11-24 00:22:40 -0500

    edit conditional rendering to use custom hook

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,21 +1,44 @@
 import React from "react";
 import "./styles.scss";
-import classNames from "classnames";
+//import classNames from "classnames";
+import useVisualMode from '../../hooks/useVisualMode';
 
 // component imports
 import Header from "./Header";
 import Show from "./Show";
 import Empty from "./Empty";
+import Form from "./Form";
 
 
 export default function Appointment(props) {
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   
   return (
     <article className="appointment">
       <Header
         time={props.time}>
       </Header>
-      {props.interview ? <Show student={props.interview.student} interviewer={props.interview.interviewer}/> : <Empty/> }
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && (
+        <Form
+          student={props.interview.student}
+          interviewer={props.interview.interviewer}
+          interviewers={[]}
+        />
+      )}
     </article>
   );
 } 
