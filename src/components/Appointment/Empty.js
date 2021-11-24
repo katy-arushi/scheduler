2a257cb7cb5419a340f00adf720daa23afcffda4
Arushi Katyal <katy.arushi@gmail.com>
2021-11-24 00:28:48 -0500

    add comment to keep track of which props are passed

--- src/components/Appointment/Empty.js
+++ src/components/Appointment/Empty.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+// ---------------- incoming props ---------------- //
+// onAdd : function - function to call when user calls add button
+
 export default function Empty(props) {
   return (
     <main className="appointment__add">
