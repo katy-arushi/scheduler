6b5416ff005f174dc828b1ba0961ede545d252ed
Arushi Katyal <katy.arushi@gmail.com>
2021-11-23 22:22:13 -0500

    finish implementing hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,5 +2,32 @@ import React, { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
-  return { mode };
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (mode, replace) => {
+    if (!replace) {
+      setMode(mode);
+      setHistory([...history, mode]);
+    } else {
+      // a combination of back and transition
+      // when true, erase the page we're currently on
+
+      const newHistory = [...history]; 
+      newHistory.pop();
+      newHistory.push(mode);
+      setHistory(newHistory); 
+      setMode(newHistory[(newHistory.length) - 1]); // always the last element of the history array
+    }
+  }
+
+  const back = () => {
+    if (history.length > 1) {
+      const newHistory = [...history]; // copying the history array
+      newHistory.pop(); // removing last element
+      setHistory(newHistory); // setting the history to the array that doesn't have the last element
+      setMode(newHistory[(newHistory.length) - 1]); // setting the mode to the new history, last element in the history array
+    }
+  }
+
+  return { mode, transition, back };
 }
