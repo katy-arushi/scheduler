32555bf11e2cf9dc0b44ea2b9ed5d66c95b78879
Arushi Katyal <katy.arushi@gmail.com>
2021-11-24 00:51:43 -0500

    add comment to keep track of which props are passed

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,12 @@ import React from "react";
 import "components/Button.scss";
 import classNames from "classnames";
 
+// ---------------- incoming props ---------------- //
+// confirm : Boolean - to set conditional CSS styles
+// danger : Boolean - to set conditional CSS styles
+// disabled : Boolean - to set conditional CSS styles
+// onClick : function - to run when a user clicks on the button. event handler
+
 export default function Button(props) {
   const buttonClass = classNames("button", {
     "button--confirm": props.confirm,
