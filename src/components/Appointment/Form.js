bb27a8c5b5d108294172ad6a81c7f14b9bf501db
Arushi Katyal <katy.arushi@gmail.com>
2021-12-06 17:28:25 -0500

    add validate function to make sure student name is entered when booking an apt

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -14,6 +14,8 @@ export default function Form(props) {
   
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
+
 
   const reset = () => {
     setStudent("")
@@ -23,6 +25,15 @@ export default function Form(props) {
   const cancel = () => {
     reset()
     props.onCancel()
+  }
+
+  function validate() {
+    if (student === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    setError("");
+    props.onSave(student, interviewer, props.isNew);
   }
 
   return (
@@ -39,6 +50,7 @@ export default function Form(props) {
             data-testid="student-name-input"
           />
         </form>
+        <section className="appointment__validation">{error}</section>
         <InterviewerList
           interviewers={props.interviewers}
           value={interviewer}
@@ -48,7 +60,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={() => {props.onSave(student, interviewer, props.isNew)}}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
