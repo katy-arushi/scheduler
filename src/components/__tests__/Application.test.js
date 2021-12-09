5c21da267a10f22f673bd4f95fca89cc462ddaea
Arushi Katyal <katy.arushi@gmail.com>
2021-12-09 01:23:11 -0500

    add comments for first test, start writing 3rd int test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -7,12 +7,17 @@ afterEach(cleanup);
 describe('Application', () => {
   
   it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+    // 1. Render the Application.
     const { getByText } = render(<Application />);
     
+    // 2. Wait until the appointments for that Day have been rendered
+      // waiting for an async API call to the backend
     await waitForElement(() => getByText("Monday"))
     
+    // 3. Click on Tuesday in the Day List Sidebar
     fireEvent.click(getByText("Tuesday"));
     
+    // 4. Check that one of the Tuesday appointments shows up in the DOM
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
 
@@ -57,6 +62,17 @@ describe('Application', () => {
 
     // 9. Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
-  })
+  });
+
+  it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
+    // 1. Render the Application.
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    // 3. Click the "Delete" button on the booked appointment.
+    // 4. Check that the confirmation message is shown.
+    // 5. Click the "Confirm" button on the confirmation.
+    // 6. Check that the element with the text "Deleting" is displayed.
+    // 7. Wait until the element with the "Add" button is displayed.
+    // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
+  });
 });
 
