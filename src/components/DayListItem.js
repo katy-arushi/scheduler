10943edf04d817ede664074d25e7913d9957e865
Arushi Katyal <katy.arushi@gmail.com>
2021-12-09 12:33:49 -0500

    edit props, delete empty line

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,7 +8,6 @@ import classNames from "classnames";
 // selected : Boolean - true or false declaring that this day is selected
 // setDay : Function - accepts the name of the day eg. "Monday", "Tuesday"
 
-
 export default function DayListItem(props) {
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
@@ -32,7 +31,7 @@ export default function DayListItem(props) {
       data-testid="day"
     >
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{formatSpots()}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
