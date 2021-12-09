ccf9b58501aed1ce85f6111648ca685803522030
Arushi Katyal <katy.arushi@gmail.com>
2021-12-09 01:52:39 -0500

    edit line spacing

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -83,6 +83,10 @@ export default {
     return Promise.resolve(
       { status: 204, statusText: "No Content" }
     );
-
+  }),
+  delete: jest.fn(() => {
+    return Promise.resolve(
+      { status: 204, statusText: "No Content" }
+    );
   })
 }
